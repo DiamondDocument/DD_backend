a9652f1cb66c5ae14024fd0f4ad0a544ed0b7827
MaxLong2001 <20373785@buaa.edu.cn>
2022-05-19 21:08:18 +0800

    lyz: finish MessageServiceImpl

--- src/main/java/diamondpick/dd_backend/Service/lyz/MessageServiceImpl.java
+++ src/main/java/diamondpick/dd_backend/Service/lyz/MessageServiceImpl.java
@@ -18,37 +18,89 @@ public class MessageServiceImpl implements MessageService {
     private TeamDao teamDao;
 
     @Override
-    public boolean newInviteMsg(String teamId, String receiverId) {
-        String senderId = teamDao.selectCaptainID(Integer.parseInt(teamId));
-        Message message = new Message();
-        return false;
-    }
-
-    @Override
-    public boolean newInviteDealMsg(boolean isAccept, String dealerId, String teamId) {
-
-        return isAccept;
+    public boolean newApplyMsg(String senderId, String teamId) {
+        try {
+            String receiverId = teamDao.selectCaptainID(teamId);
+            Message message = new Message(senderId, receiverId, 2, null, null, teamId, 0);
+            messageDao.sendNewMessage(message);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean newApplyDealMsg(boolean isAccept, String dealerId, String teamId) {
-
-        return false;
+        try {
+            String senderId = teamDao.selectCaptainID(teamId);
+            String receiverId = dealerId;
+            Message message;
+            if (isAccept)
+                message = new Message(senderId, receiverId, 3, null, null, teamId, 1);
+            else
+                message = new Message(senderId, receiverId, 3, null, null, teamId, 2);
+            messageDao.sendNewMessage(message);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
-    public boolean newApplyMsg(String senderId, String teamId) {
-        return false;
+    public boolean newInviteMsg(String teamId, String receiverId) {
+        try {
+            String senderId = teamDao.selectCaptainID(teamId);
+            Message message = new Message(senderId, receiverId, 4, null, null, teamId, 0);
+            messageDao.sendNewMessage(message);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean newInviteDealMsg(boolean isAccept, String dealerId, String teamId) {
+        try {
+            String senderId = dealerId;
+            String receiverId = teamDao.selectCaptainID(teamId);
+            Message message;
+            if (isAccept)
+                message = new Message(senderId, receiverId, 5, null, null, teamId, 1);
+            else
+                message = new Message(senderId, receiverId, 5, null, null, teamId, 2);
+            messageDao.sendNewMessage(message);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean newCommentMsg(String senderId, String docId, String receiverId) {
+        try {
+            Message message = new Message(senderId, receiverId, 6, null, docId, null, 0);
+            messageDao.sendNewMessage(message);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean newAtMsg(String senderId, String docId, String receiverId) {
-        return false;
-    }
-
-    @Override
-    public boolean newCommentMsg(String senderId, String docId) {
-        return false;
+        try {
+            Message message = new Message(senderId, receiverId, 7, null, docId, null, 0);
+            messageDao.sendNewMessage(message);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
