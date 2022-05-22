c24f38083f9cb38f1799b3ac627e8ed7bfacad8f
evoLonation <1838940019@qq.com>
2022-05-22 12:33:18 +0800

    zzy：新增了几个测试接口

--- src/main/java/diamondpick/dd_backend/Controller/zzy/HelloController.java
+++ src/main/java/diamondpick/dd_backend/Controller/zzy/HelloController.java
@@ -1,13 +1,28 @@
 package diamondpick.dd_backend.Controller.zzy;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
     @GetMapping("api/hello")
-    public String helloWorld(){
-        return "hello,world!";
+    public Map<String, Object> helloWorld(@RequestParam(required = false) String userId){
+        Map<String, Object> ret = new HashMap<>();
+        ret.put("giao", "一给我里giao");
+        ret.put("message", "hello,world," + userId);
+        ret.put("code", 0);
+        return ret;
+    }
+    @PostMapping("api/post")
+    public String post(@RequestBody Map<String, Object> req){
+        return (String)req.get("content");
+    }
+    @PostMapping("api/file")
+    public String file(@RequestParam MultipartFile file){
+        return "获得文件"+file.getName();
     }
 }
