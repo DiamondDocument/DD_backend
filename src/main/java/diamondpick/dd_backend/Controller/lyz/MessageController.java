e1ec3d6539beaa9104c6e334befa52b9db9262a1
MaxLong2001 <20373785@buaa.edu.cn>
2022-05-23 00:10:59 +0800

    lyz: /api/message/list

--- src/main/java/diamondpick/dd_backend/Controller/lyz/MessageController.java
+++ src/main/java/diamondpick/dd_backend/Controller/lyz/MessageController.java
@@ -1,16 +1,17 @@
 package diamondpick.dd_backend.Controller.lyz;
 
+import com.alibaba.fastjson.JSONObject;
 import diamondpick.dd_backend.Entity.lyz.Message;
-import diamondpick.dd_backend.Entity.yyh.User;
+import diamondpick.dd_backend.Entity.yyh.TeamMessage;
+import diamondpick.dd_backend.Entity.zzy.Document;
+import diamondpick.dd_backend.Service.DocumentService;
 import diamondpick.dd_backend.Service.MessageService;
+import diamondpick.dd_backend.Service.TeamService;
 import diamondpick.dd_backend.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.alibaba.fastjson.JSONObject;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,34 +22,11 @@ public class MessageController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/api/message/send")
-    public Map<String, Object> sendMessage(@RequestBody Map<String, String> requestMap) {
-        String senderID = requestMap.get("senderId");
-        String receiverID = requestMap.get("receiverId");
-        String messageContent = requestMap.get("message");
-        Map<String, Object> map = new HashMap<>();
-//        try {
-//            User userSender = userService.selectUserByUserId(senderID);
-//            User userReceiver = userService.selectUserByUserId(receiverID);
-//            if (userSender == null) {
-//                map.put("error", 1);
-//                map.put("message", "用户不存在：发送者不存在");
-//            } else if (userReceiver == null) {
-//                map.put("error", 1);
-//                map.put("msg", "用户不存在：接收者不存在");
-//            } else {
-//                Message sendMessage = new Message(senderID, receiverID, messageContent, new Date(System.currentTimeMillis()), 1);
-//                messageService.sendNewMessage(sendMessage);
-//                map.put("error", 0);
-//                map.put("message", "发送成功");
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            map.put("error", -1);
-//            map.put("message", "其他错误");
-//        }
-        return map;
-    }
+    @Autowired
+    private TeamService teamService;
+
+    @Autowired
+    private DocumentService documentService;
 
     @GetMapping("/api/message/mark")
     public Map<String, Object> receiveMessage(@RequestParam("userId") String receiverID) {
@@ -86,35 +64,62 @@ public class MessageController {
         return map;
     }
 
-    @PostMapping("/api/message/list")
-    public Map<String, Object> listMessage(@RequestBody Map<String, String> requestMap) {
-        String userId = requestMap.get("userId");
+    @GetMapping("/api/message/list")
+    public Map<String, Object> listMessage(@RequestParam("userId") String userId, @RequestParam("msgType") int msgType) {
+        Map<String, Object> map = new HashMap<>();
+        try {
+            ArrayList<JSONObject> jsonList = new ArrayList<>();
+            ArrayList<Message> messages = messageService.listMessage(userId);
+            map.put("code", 0);
+            for (Message message : messages) {
+                if (msgType != 0 && message.getMsgType() != msgType)
+                    continue;
+                JSONObject json = new JSONObject();
+                json.put("msgId", message.getMessageId());
+                json.put("msgType", message.getMsgType());
+                json.put("senderId", message.getSenderId());
+                json.put("senderName", userService.selectUserByUserId(message.getSenderId()).getNickname());
+                json.put("sendTime", message.getSendTime());
+                if (message.getMsgType() == 2 || message.getMsgType() == 3 || message.getMsgType() == 4 || message.getMsgType() == 5) {
+                    TeamMessage teamMessage = teamService.selectTeamByTeamId(message.getTeamId());
+                    json.put("teamName", teamMessage.getTeamName());
+                }
+                if (message.getMsgType() == 6 || message.getMsgType() == 7) {
+                    Document document = documentService.selectDocByDocId(message.getMsgDocId());
+                    json.put("msgDocName", document.getName());
+                }
+                jsonList.add(json);
+            }
+            map.put("error", 0);
+            map.put("msg", jsonList);
+        } catch (Exception e) {
+            e.printStackTrace();
+            map.put("code", -1);
+            map.put("message", "其他错误");
+        }
+        return map;
+    }
+
+    @PostMapping("/api/message/send")
+    public Map<String, Object> sendMessage(@RequestBody Map<String, String> requestMap) {
+        String senderID = requestMap.get("senderId");
+        String receiverID = requestMap.get("receiverId");
+        String messageContent = requestMap.get("message");
         Map<String, Object> map = new HashMap<>();
 //        try {
-//            User user = userService.selectUserByUserId(userId);
-//            if (user == null) {
+//            User userSender = userService.selectUserByUserId(senderID);
+//            User userReceiver = userService.selectUserByUserId(receiverID);
+//            if (userSender == null) {
 //                map.put("error", 1);
-//                map.put("message", "用户不存在：接收者不存在");
+//                map.put("message", "用户不存在：发送者不存在");
+//            } else if (userReceiver == null) {
+//                map.put("error", 1);
+//                map.put("msg", "用户不存在：接收者不存在");
 //            } else {
-//                ArrayList<Message> messages;
-//                messages = messageService.listMessage(userId);
-//                if (messages.size() == 0) {
-//                    map.put("error", 0);
-//                    map.put("message", "当前用户没有新通知");
-//                } else {
-//                    ArrayList<JSONObject> jsonList = new ArrayList<>();
-//                    for (Message message : messages) {
-//                        JSONObject jsonObject = new JSONObject();
-//                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd.HH.mm");
-//                        jsonObject.put("content", message.getMessage());
-//                        jsonObject.put("date", formatter.format(message.getDateCreated()));
-//                        jsonObject.put("status", message.getStatus());
-//                        jsonList.add(jsonObject);
-//                    }
-//                    map.put("error", 0);
-//                    map.put("message", jsonList);
-//                    messageService.changeMessageStatus(userId, 2, 3);
-//                }
+//                Message sendMessage = new Message(senderID, receiverID, messageContent, new Date(System.currentTimeMillis()), 1);
+//                messageService.sendNewMessage(sendMessage);
+//                map.put("error", 0);
+//                map.put("message", "发送成功");
 //            }
 //        } catch (Exception e) {
 //            e.printStackTrace();
