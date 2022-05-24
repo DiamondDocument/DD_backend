d0d3f8d7fa37c8eab09e9bf35f3a235c1d19d4db
evoLonation <1838940019@qq.com>
2022-05-24 12:10:58 +0800

    zzy：添加异常

--- src/main/java/diamondpick/dd_backend/Service/UserService.java
+++ src/main/java/diamondpick/dd_backend/Service/UserService.java
@@ -1,6 +1,7 @@
 package diamondpick.dd_backend.Service;
 
 import diamondpick.dd_backend.Entity.yyh.User;
+import diamondpick.dd_backend.zzy.Exception.UserNotExist;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,7 +9,7 @@ public interface UserService {
 
 
     //若为null则意味着不存在
-    public User selectUserByUserId(String id);
+    public User selectUserByUserId(String id)throws UserNotExist;
     //插入失败则返回false
     public boolean addUser(User user);
     boolean isEmailExist(String email);
