fb3d36cff53745a1c7e2f9fd8d3c9eb2cc38364c
evoLonation <1838940019@qq.com>
2022-05-27 22:23:04 +0800

    zzy：添加权限修改接口

--- src/main/java/diamondpick/dd_backend/Dao/FolderDao.java
+++ src/main/java/diamondpick/dd_backend/Dao/FolderDao.java
@@ -19,29 +19,32 @@ public interface FolderDao {
     public void updateFolder(String folderId, String key, Object newValue)throws DataIntegrityViolationException, BadSqlGrammarException;
 
     /**将文件夹设置为删除状态，注意更新delete_time*/
-    public void updateToDelete(String folderId, String deleterId);
+    public void updateToDelete(String folderId, String deleterId)throws DataIntegrityViolationException;
+
+    /** 更新parentId代表的文件夹的子目录下的所有文件夹的now_auth（取自身的和输入参数中的最小值）*/
+    public void updateSubDirAuth(String parentId, int newAuth)throws DataIntegrityViolationException;
 
     /**
      * @return 注意这里包括有创建者名称
      */
     public Folder selectFolder(String folderId);
     /**
-     * @return 注意这里包括有创建者名称
+     * @return 注意这里包括有创建者名称，并且只有未删除的文件夹
      */
     public List<Folder> selectSubDir(String parentId);
     /**
-     * @return 注意这里包括有创建者名称
+     * @param type 可以是"team"和"user"的一种，表示是团队空间还是用户空间
+     * @param spaceOwnerId 空间所有者的id（可能是团队id或者用户id）
+     * @return 只包括未删除的，返回的对象包括有创建者名称
      */
-    public List<Folder> selectRootDirInUSpace(String userId);
+    public List<Folder> selectRootDir(String type, String spaceOwnerId)throws BadSqlGrammarException;
+
     /**
-     * @return 注意这里包括有创建者名称
+     * @param type 可以是"team"和"user"的一种，表示是团队空间还是用户空间
+     * @param spaceOwnerId 空间所有者的id（可能是团队id或者用户id）
+     * @return 已删除的文件夹，返回的对象包括有创建者名称
      */
-    public List<Folder> selectRootDirInTSpace(String teamId);
+    public List<Folder> selectDeleted(String type, String spaceOwnerId)throws BadSqlGrammarException;
 
-    /**@return 返回delete为1的某用户空间的所有文件夹*/
-    public List<Folder> selectDeletedInUSpace(String userId);
-
-    /**@return 返回delete为1的某团队空间的所有文件夹，需要删除者名称*/
-    public List<Folder> selectDeletedInTSpace(String teamId);
 
 }
