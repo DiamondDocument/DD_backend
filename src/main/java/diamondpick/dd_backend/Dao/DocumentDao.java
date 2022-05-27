56756f2ccff4e98ec90f7c2485d683f74145b77c
evoLonation <1838940019@qq.com>
2022-05-27 20:47:39 +0800

    zzy：初步完成DocumentDao和Doc实体

--- src/main/java/diamondpick/dd_backend/Dao/DocumentDao.java
+++ src/main/java/diamondpick/dd_backend/Dao/DocumentDao.java
@@ -17,33 +17,35 @@ public interface DocumentDao {
             "    values(#{param1},#{param2},#{param3},#{param4},#{param4},#{param5}, #{param6})")
     public void insertDoc(String docId, String name, String creatorId, int authority, String parentId, String spaceId)throws DuplicateKeyException, DataIntegrityViolationException;
 
-
+    @Insert("insert into document_collector values(#{param2}, #{param1}")
     public void insertCollection(String collectorId, String docId)throws DataIntegrityViolationException;
 
+
     /**如果这个报异常，可以捕获异常后插入*/
+    @Insert("insert into user_recent values(#{param2}, #{param1})")
     public void insertRecent(String browserId, String docId)throws DataIntegrityViolationException;
 
     @Delete("delete from documents where doc_id = #{param1}")
     public void deleteDoc(String docId);
 
+    @Delete("delete from document_collector where collector_id = #{param1} and doc_id = #{param2}")
     public void deleteCollection(String collectorId, String docId);
 
     @Update("update documents set ${key} = #{value} where doc_id = #{docId}")
     public void updateDoc(@Param("docId")String docId, @Param("key")String key, @Param("value")Object value)throws DataIntegrityViolationException, BadSqlGrammarException;
 
-    /** 更新至最新 */
+    /** 更新浏览时间至现在时间 */
+    @Update("update user_recent set browse_time = now() where browser_id = #{param1} and doc_id = #{param2}")
     public void updateRecent(String browserId, String docId)throws DataIntegrityViolationException;
 
-    /**
-     * @return 注意这里包括有创建者和修改者的名称
-     */
-    @Select("select * from documents where doc_id = #{param1}")
+    /**@return 注意这里包括有创建者、修改者和删除者的名称*/
+    @Select("select * from documents as doc, users as m, users as c " +
+            " where doc_id = #{param1} and " +
+            "doc.modifier_id = m.user_id and " +
+            "doc.creator_id = c.user_id ")
     public Document selectDoc(String docId);
-    /*
-    @Select("select * from documents where ${key} = #{value}")
-    @ResultType(Document.class)
-    public List<Document> selectDocBy(@Param("key")String key, @Param("value")Object value);
-     */
+
+    @Select("select * from documents")
     public List<Document> selectCollection(String collectorId);
     public List<Document> selectSubdirectory(String parentId);
     public List<Document> selectRootDirectoryUSpace(String userId);
@@ -55,5 +57,11 @@ public interface DocumentDao {
     @Select("select count(*) from documents")
     public Integer numOfDoc();
 
+
+    /*
+    @Select("select * from documents where ${key} = #{value}")
+    @ResultType(Document.class)
+    public List<Document> selectDocBy(@Param("key")String key, @Param("value")Object value);
+     */
 
 }
