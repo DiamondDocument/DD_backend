b589694e250d94789bd7f366bcad0bd1bfd3a01b
evoLonation <1838940019@qq.com>
2022-05-27 22:52:59 +0800

    zzy：完善SearchDao接口定义

--- src/main/java/diamondpick/dd_backend/Dao/SearchDao.java
+++ src/main/java/diamondpick/dd_backend/Dao/SearchDao.java
@@ -4,13 +4,22 @@ import diamondpick.dd_backend.Entity.Document;
 import diamondpick.dd_backend.Old.yyh.Entity.Team;
 import diamondpick.dd_backend.Old.yyh.Entity.User;
 import diamondpick.dd_backend.Entity.Folder;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface SearchDao {
-    //根据用户信息模糊查找所有用户
-    public List<User> selectUserVaguely(String value);
+
+    /**根据用户信息模糊查找*/
+    @Select("select * from users where user_id like '%${value}%' or nickname like '%${value}%'")
+    public List<User> selectUser(String value);
+    /**根据用户信息模糊查找*/
+    @Select("select * from teams where team_id like '%${value}%' or name like '%${value}%'")
     public List<Team> searchTeam(String value);
+    /**根据用户信息模糊查找*/
+    @Select("select * from documents where doc_id like '%${value}%' or name like '%${value}%'")
     public List<Document> searchDoc(String value);
+    /**根据用户信息模糊查找*/
+    @Select("select * from folders where folder_id like '%${value}%' or name like '%${value}%'")
     public List<Folder> searchFolder(String value);
 }
