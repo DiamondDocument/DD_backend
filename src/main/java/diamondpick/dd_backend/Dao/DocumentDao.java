4640ff283944f14bc5c45b8a210aab9ce40f7bfc
evoLonation <1838940019@qq.com>
2022-05-27 22:00:32 +0800

    zzy：初步完成DocumentDao的所有接口

--- src/main/java/diamondpick/dd_backend/Dao/DocumentDao.java
+++ src/main/java/diamondpick/dd_backend/Dao/DocumentDao.java
@@ -17,12 +17,12 @@ public interface DocumentDao {
             "    values(#{param1},#{param2},#{param3},#{param4},#{param4},#{param5}, #{param6})")
     public void insertDoc(String docId, String name, String creatorId, int authority, String parentId, String spaceId)throws DuplicateKeyException, DataIntegrityViolationException;
 
-    @Insert("insert into document_collector values(#{param2}, #{param1}")
+    @Insert("insert into document_collector values(#{param2}, #{param1})")
     public void insertCollection(String collectorId, String docId)throws DataIntegrityViolationException;
 
 
     /**如果这个报异常，可以捕获异常后插入*/
-    @Insert("insert into user_recent values(#{param2}, #{param1})")
+    @Insert("insert into user_recent(doc_id, user_id) values(#{param2}, #{param1})")
     public void insertRecent(String browserId, String docId)throws DataIntegrityViolationException;
 
     @Delete("delete from documents where doc_id = #{param1}")
@@ -34,34 +34,83 @@ public interface DocumentDao {
     @Update("update documents set ${key} = #{value} where doc_id = #{docId}")
     public void updateDoc(@Param("docId")String docId, @Param("key")String key, @Param("value")Object value)throws DataIntegrityViolationException, BadSqlGrammarException;
 
+    @Update("update documents set is_delete = true , deleter_id = #{param2} , delete_time = now() where doc_id = #{param1}")
+    public void updateToDelete(String docId, String deleterId)throws DataIntegrityViolationException;
+
     /** 更新浏览时间至现在时间 */
-    @Update("update user_recent set browse_time = now() where browser_id = #{param1} and doc_id = #{param2}")
+    @Update("update user_recent set browse_time = now() where user_id = #{param1} and doc_id = #{param2}")
     public void updateRecent(String browserId, String docId)throws DataIntegrityViolationException;
 
-    /**@return 注意这里包括有创建者、修改者和删除者的名称*/
-    @Select("select * from documents as doc, users as m, users as c " +
-            " where doc_id = #{param1} and " +
+    /** @return 注意这里包括有创建者、修改者和删除者的昵称 */
+    @Select("select doc.*, m.nickname as modifier_name, c.nickname as creator_name from documents as doc, users as m, users as c " +
+            "where doc_id    = #{param1} and " +
             "doc.modifier_id = m.user_id and " +
-            "doc.creator_id = c.user_id ")
+            "doc.creator_id  = c.user_id ")
     public Document selectDoc(String docId);
 
-    @Select("select * from documents")
+
+    /** @return 只包括未删除的 */
+    @Select("select doc.*, m.nickname as modifier_name, c.nickname as creator_name " +
+            "from documents as doc, users as m, users as c, document_collector as co " +
+            " where co.collector_id = #{param1} and " +
+            " co.doc_id = doc.doc_id and " +
+            "doc.modifier_id = m.user_id and " +
+            "doc.creator_id = c.user_id and " +
+            "doc.is_delete = false")
     public List<Document> selectCollection(String collectorId);
-    public List<Document> selectSubdirectory(String parentId);
-    public List<Document> selectRootDirectoryUSpace(String userId);
-    public List<Document> selectRootDirectoryTSpace(String teamId);
 
 
+    @Select("select doc.*, m.nickname as modifier_name, c.nickname as creator_name " +
+            "from documents as doc, users as m, users as c  " +
+            " where doc.parent_id = #{param1} and " +
+            "doc.modifier_id = m.user_id and " +
+            "doc.creator_id = c.user_id and  " +
+            "doc.is_delete = false           " )
+    /** 只包括未删除的 */
+    public List<Document> selectSubDir(String parentId);
+
+    /**
+     * @param type 可以是"team"和"user"的一种，表示是团队空间还是用户空间
+     * @param spaceOwnerId 空间所有者的id（可能是团队id或者用户id）
+     * @return 只包括未删除的
+     */
+    @Select("select doc.*, m.nickname as modifier_name, c.nickname as creator_name " +
+            "from documents as doc, users as m, users as c, ${param1}s as s  " +
+            "where " +
+            "s.${param1}_id            = #{param1} and      " +
+            "s.space_id           = doc.space_id and   " +
+            "doc.modifier_id      = m.user_id and      " +
+            "doc.creator_id       = c.user_id and      " +
+            "doc.is_delete        = false              " )
+    public List<Document> selectRootDir(String type, String spaceOwnerId);
+
+    @Select("select doc.*, d.nickname as deleter_name, c.nickname as creator_name " +
+            "from documents as doc, users as d, users as c, ${param1}s as s  " +
+            "where " +
+            "s.${param1}_id            = #{param2} and      " +
+            "s.space_id           = doc.space_id and   " +
+            "doc.deleter_id       = d.user_id and      " +
+            "doc.creator_id       = c.user_id and      " +
+            "doc.is_delete        = true               " )
+    /**这里的额外信息没有修改者的名称，只有创建者和删除者的名称 */
+    public List<Document> selectDeleted(String type, String spaceOwnerId);
+
+    @Select("select doc.*, m.nickname as modifier_name, c.nickname as creator_name " +
+            "from documents as doc, users as m, users as c, user_recent as r  " +
+            "where " +
+            "r.user_id            = #{param1} and      " +
+            "s.space_id           = doc.space_id and   " +
+            "doc.modifier_id      = m.user_id and      " +
+            "doc.creator_id       = c.user_id and      " +
+            "doc.doc_id           = r.doc_id           " +
+            "doc.is_delete        = false              " +
+            "order by r.browse_time desc               " +
+            "limit  ${param2}                          " )
+    /** 只包括未删除的，按照时间排序，如果多于limit个则显示limit个 */
     public List<Document> selectRecent(String browserId, int limit);
 
+
     @Select("select count(*) from documents")
-    public Integer numOfDoc();
-
-
-    /*
-    @Select("select * from documents where ${key} = #{value}")
-    @ResultType(Document.class)
-    public List<Document> selectDocBy(@Param("key")String key, @Param("value")Object value);
-     */
+    public int numOfDoc();
 
 }
