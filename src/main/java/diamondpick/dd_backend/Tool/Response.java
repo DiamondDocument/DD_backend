c5d4f5e91d8c07c033ea4c802559826e8ac95722
evoLonation <1838940019@qq.com>
2022-06-04 08:11:55 +0800

    zzy：tianjiaResponse的说明

--- src/main/java/diamondpick/dd_backend/Tool/Response.java
+++ src/main/java/diamondpick/dd_backend/Tool/Response.java
@@ -2,7 +2,24 @@ package diamondpick.dd_backend.Tool;
 
 import java.util.HashMap;
 
-
+/**
+ * 使用方式：
+ * 假设要返回如下json：
+ * <pre>
+ *   {
+ *      "code" : 0
+ *      "name" : "giao",
+ *      "id" : "123
+ *   }
+ * </pre>
+ * 只需要：
+ * <pre>
+ *     Response r = new Response("name", "id");
+ *     return r.get(0, "giao", 123);
+ *     // 可以使用同一个Response对象模板返回多个不同值的json
+ *     return r.get("zzy", 124);
+ * </pre>
+ */
 public class Response{
     public Response(String... key){
         this.keys = key;
