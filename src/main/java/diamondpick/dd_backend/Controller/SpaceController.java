38e8709cdcbf86187beefe275c7e54dca5935107
MaxLong2001 <20373785@buaa.edu.cn>
2022-06-08 18:36:19 +0800

    lyz: 修改空间内容、收藏文档内容、最近浏览内容的返回值

--- src/main/java/diamondpick/dd_backend/Controller/SpaceController.java
+++ src/main/java/diamondpick/dd_backend/Controller/SpaceController.java
@@ -4,12 +4,14 @@ import com.alibaba.fastjson.JSONObject;
 import diamondpick.dd_backend.Dao.*;
 import diamondpick.dd_backend.Entity.Document;
 import diamondpick.dd_backend.Entity.Folder;
+import diamondpick.dd_backend.Exception.OperationFail;
 import diamondpick.dd_backend.Service.AuthService;
 import diamondpick.dd_backend.Service.DocumentService;
 import diamondpick.dd_backend.Tool.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -49,6 +51,7 @@ public class SpaceController {
             if (isBack && folderId == null) {
                 throw new Exception("isBack is true, but folderId is null.");
             }
+
             if (isBack) {
                 res = new Response("parentId", "files");
                 String parentId = folderDao.selectFolder(folderId).getParentId();
@@ -57,39 +60,15 @@ public class SpaceController {
                     List<Document> documents = documentDao.selectRootDir(type, ownerId);
                     folders.sort((o1, o2) -> o2.getName().compareTo(o1.getName()));
                     documents.sort((o1, o2) -> o2.getName().compareTo(o1.getName()));
-                    for (Folder folder : folders) {
-                        if (authService.checkFileAuth(folder.getFileId(), visitorId) >= 2) {
-                            JSONObject json = new JSONObject();
-                            json.put("fileType", 2);
-                            json.put("fileId", folder.getFileId());
-                            json.put("fileName", folder.getName());
-                            json.put("creatorId", folder.getCreatorId());
-                            json.put("creatorName", folder.getCreatorName());
-                            json.put("createTime", folder.getCreateTime());
-                            jsonList.add(json);
-                        }
-                    }
-                    for (Document document : documents) {
-                        if (authService.checkFileAuth(document.getFileId(), visitorId) >= 2) {
-                            JSONObject json = new JSONObject();
-                            json.put("fileType", 1);
-                            json.put("fileId", document.getFileId());
-                            json.put("fileName", document.getName());
-                            json.put("creatorId", document.getCreatorId());
-                            json.put("creatorName", document.getCreatorName());
-                            json.put("createTime", document.getCreateTime());
-                            json.put("modifierId", document.getModifierId());
-                            json.put("modifierName", document.getModifierName());
-                            json.put("modifyTime", document.getModifyTime());
-                            json.put("size", documentService.getSize(document.getFileId()));
-                            jsonList.add(json);
-                        }
-                    }
+
+                    addFolderToJsonList(jsonList, folders, visitorId, -1);
+                    addDocumentToJsonList(jsonList, documents, visitorId, -1);
                 } else {
                     List<Folder> folders = folderDao.selectSubDir(parentId);
                     List<Document> documents = documentDao.selectSubDir(parentId);
                     folders.sort((o1, o2) -> o2.getName().compareTo(o1.getName()));
                     documents.sort((o1, o2) -> o2.getName().compareTo(o1.getName()));
+
                     int spaceId;
                     if (type.equals("user")) {
                         spaceId = userDao.selectUser(ownerId).getSpaceId();
@@ -98,36 +77,9 @@ public class SpaceController {
                     } else {
                         throw new Exception();
                     }
-                    for (Folder folder : folders) {
-                        if (authService.checkFileAuth(folder.getFileId(), visitorId) >= 2 &&
-                                folder.getSpaceId().equals(String.valueOf(spaceId))) {
-                            JSONObject json = new JSONObject();
-                            json.put("fileType", 2);
-                            json.put("fileId", folder.getFileId());
-                            json.put("fileName", folder.getName());
-                            json.put("creatorId", folder.getCreatorId());
-                            json.put("creatorName", folder.getCreatorName());
-                            json.put("createTime", folder.getCreateTime());
-                            jsonList.add(json);
-                        }
-                    }
-                    for (Document document : documents) {
-                        if (authService.checkFileAuth(document.getFileId(), visitorId) >= 2 &&
-                                document.getSpaceId().equals(String.valueOf(spaceId))) {
-                            JSONObject json = new JSONObject();
-                            json.put("fileType", 1);
-                            json.put("fileId", document.getFileId());
-                            json.put("fileName", document.getName());
-                            json.put("creatorId", document.getCreatorId());
-                            json.put("creatorName", document.getCreatorName());
-                            json.put("createTime", document.getCreateTime());
-                            json.put("modifierId", document.getModifierId());
-                            json.put("modifierName", document.getModifierName());
-                            json.put("modifyTime", document.getModifyTime());
-                            json.put("size", document.getSize());
-                            jsonList.add(json);
-                        }
-                    }
+
+                    addFolderToJsonList(jsonList, folders, visitorId, spaceId);
+                    addDocumentToJsonList(jsonList, documents, visitorId, spaceId);
                 }
                 return res.get(0, parentId, jsonList);
             } else {
@@ -137,39 +89,15 @@ public class SpaceController {
                     List<Document> documents = documentDao.selectRootDir(type, ownerId);
                     folders.sort((o1, o2) -> o2.getName().compareTo(o1.getName()));
                     documents.sort((o1, o2) -> o2.getName().compareTo(o1.getName()));
-                    for (Folder folder : folders) {
-                        if (authService.checkFileAuth(folder.getFileId(), visitorId) >= 2) {
-                            JSONObject json = new JSONObject();
-                            json.put("fileType", 2);
-                            json.put("fileId", folder.getFileId());
-                            json.put("fileName", folder.getName());
-                            json.put("creatorId", folder.getCreatorId());
-                            json.put("creatorName", folder.getCreatorName());
-                            json.put("createTime", folder.getCreateTime());
-                            jsonList.add(json);
-                        }
-                    }
-                    for (Document document : documents) {
-                        if (authService.checkFileAuth(document.getFileId(), visitorId) >= 2) {
-                            JSONObject json = new JSONObject();
-                            json.put("fileType", 1);
-                            json.put("fileId", document.getFileId());
-                            json.put("fileName", document.getName());
-                            json.put("creatorId", document.getCreatorId());
-                            json.put("creatorName", document.getCreatorName());
-                            json.put("createTime", document.getCreateTime());
-                            json.put("modifierId", document.getModifierId());
-                            json.put("modifierName", document.getModifierName());
-                            json.put("modifyTime", document.getModifyTime());
-                            json.put("size", documentService.getSize(document.getFileId()));
-                            jsonList.add(json);
-                        }
-                    }
+
+                    addFolderToJsonList(jsonList, folders, visitorId, -1);
+                    addDocumentToJsonList(jsonList, documents, visitorId, -1);
                 } else {
                     List<Folder> folders = folderDao.selectSubDir(folderId);
                     List<Document> documents = documentDao.selectSubDir(folderId);
                     folders.sort((o1, o2) -> o2.getName().compareTo(o1.getName()));
                     documents.sort((o1, o2) -> o2.getName().compareTo(o1.getName()));
+
                     int spaceId;
                     if (type.equals("user")) {
                         spaceId = userDao.selectUser(ownerId).getSpaceId();
@@ -178,36 +106,9 @@ public class SpaceController {
                     } else {
                         throw new Exception();
                     }
-                    for (Folder folder : folders) {
-                        if (authService.checkFileAuth(folder.getFileId(), visitorId) >= 2 &&
-                                folder.getSpaceId().equals(String.valueOf(spaceId))) {
-                            JSONObject json = new JSONObject();
-                            json.put("fileType", 2);
-                            json.put("fileId", folder.getFileId());
-                            json.put("fileName", folder.getName());
-                            json.put("creatorId", folder.getCreatorId());
-                            json.put("creatorName", folder.getCreatorName());
-                            json.put("createTime", folder.getCreateTime());
-                            jsonList.add(json);
-                        }
-                    }
-                    for (Document document : documents) {
-                        if (authService.checkFileAuth(document.getFileId(), visitorId) >= 2 &&
-                                document.getSpaceId().equals(String.valueOf(spaceId))) {
-                            JSONObject json = new JSONObject();
-                            json.put("fileType", 1);
-                            json.put("fileId", document.getFileId());
-                            json.put("fileName", document.getName());
-                            json.put("creatorId", document.getCreatorId());
-                            json.put("creatorName", document.getCreatorName());
-                            json.put("createTime", document.getCreateTime());
-                            json.put("modifierId", document.getModifierId());
-                            json.put("modifierName", document.getModifierName());
-                            json.put("modifyTime", document.getModifyTime());
-                            json.put("size", document.getSize());
-                            jsonList.add(json);
-                        }
-                    }
+
+                    addFolderToJsonList(jsonList, folders, visitorId, spaceId);
+                    addDocumentToJsonList(jsonList, documents, visitorId, spaceId);
                 }
                 return res.get(0, jsonList);
             }
@@ -243,21 +144,8 @@ public class SpaceController {
             List<JSONObject> jsonList = new ArrayList<>();
             List<Document> documents = documentDao.selectCollection(userId);
             documents.sort((o1, o2) -> o2.getName().compareTo(o1.getName()));
-            for (Document document : documents) {
-                JSONObject json = new JSONObject();
-                if (authService.checkFileAuth(document.getFileId(), visitorId) >= 2) {
-                    json.put("fileId", document.getFileId());
-                    json.put("fileName", document.getName());
-                    json.put("creatorId", document.getCreatorId());
-                    json.put("creatorName", document.getCreatorName());
-                    json.put("createTime", document.getCreateTime());
-                    json.put("modifierId", document.getModifierId());
-                    json.put("modifierName", document.getModifierName());
-                    json.put("modifyTime", document.getModifyTime());
-                    json.put("size", documentService.getSize(document.getFileId()));
-                    jsonList.add(json);
-                }
-            }
+
+            addDocumentToJsonList(jsonList, documents, visitorId, -1);
             return res.get(0, jsonList);
         } catch (Exception e) {
             return res.get(-1);
@@ -313,25 +201,68 @@ public class SpaceController {
         try {
             List<JSONObject> jsonList = new ArrayList<>();
             List<Document> documents = documentDao.selectRecent(userId, 10);
-            for (Document document : documents) {
-                JSONObject json = new JSONObject();
-                if (authService.checkFileAuth(document.getFileId(), userId) >= 2) {
-                    json.put("fileId", document.getFileId());
-                    json.put("fileName", document.getName());
-                    json.put("creatorId", document.getCreatorId());
-                    json.put("creatorName", document.getCreatorName());
-                    json.put("createTime", document.getCreateTime());
-                    json.put("modifierId", document.getModifierId());
-                    json.put("modifierName", document.getModifierName());
-                    json.put("modifyTime", document.getModifyTime());
-                    json.put("browserTime", document.getBrowseTime());
-                    json.put("size", documentService.getSize(document.getFileId()));
-                    jsonList.add(json);
-                }
-            }
+
+            addDocumentToJsonList(jsonList, documents, userId, -1);
             return res.get(0, jsonList);
         } catch (Exception e) {
             return res.get(-1);
+        }
+    }
+
+    private void addFolderToJsonList(List<JSONObject> jsonList, List<Folder> folders, String visitorId, int spaceId) throws OperationFail {
+        for (Folder folder : folders) {
+            if (authService.checkFileAuth(folder.getFileId(), visitorId) >= 2 &&
+                    (spaceId == -1 || folder.getSpaceId().equals(String.valueOf(spaceId)))) {
+                JSONObject json = new JSONObject();
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                String creatorInfo = formatter.format(folder.getCreateTime()) + " by " + folder.getCreatorName();
+                json.put("fileType", 2);
+                json.put("fileId", folder.getFileId());
+                json.put("fileName", folder.getName());
+                json.put("creatorInfo", creatorInfo);
+                json.put("creatorId", folder.getCreatorId());
+                json.put("creatorName", folder.getCreatorName());
+                json.put("createTime", folder.getCreateTime());
+                json.put("authority", authService.checkFileAuth(folder.getFileId(), visitorId));
+                jsonList.add(json);
+            }
+        }
+    }
+
+    private void addDocumentToJsonList(List<JSONObject> jsonList, List<Document> documents, String visitorId, int spaceId) throws OperationFail {
+        for (Document document : documents) {
+            if (authService.checkFileAuth(document.getFileId(), visitorId) >= 2 &&
+                    (spaceId == -1 || document.getSpaceId().equals(String.valueOf(spaceId)))) {
+                JSONObject json = new JSONObject();
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                String creatorInfo = formatter.format(document.getCreateTime()) + " by " + document.getCreatorName();
+                String modifyInfo;
+                if (document.getModifyTime() == null) {
+                    modifyInfo = "No modification";
+                } else {
+                    modifyInfo = formatter.format(document.getModifyTime()) + " by " + document.getModifierName();
+                }
+                json.put("fileType", 1);
+                json.put("fileId", document.getFileId());
+                json.put("fileName", document.getName());
+                json.put("creatorInfo", creatorInfo);
+                json.put("creatorId", document.getCreatorId());
+                json.put("creatorName", document.getCreatorName());
+                json.put("createTime", document.getCreateTime());
+                json.put("modifyInfo", modifyInfo);
+                json.put("modifierId", document.getModifierId());
+                json.put("modifierName", document.getModifierName());
+                json.put("modifyTime", document.getModifyTime());
+                json.put("size", documentService.getSize(document.getFileId()));
+                json.put("authority", authService.checkFileAuth(document.getFileId(), visitorId));
+                int isShared = documentService.checkShare(document.getFileId());
+                if (isShared == 1) {
+                    json.put("isShared", 0);
+                } else {
+                    json.put("isShared", isShared);
+                }
+                jsonList.add(json);
+            }
         }
     }
 }
