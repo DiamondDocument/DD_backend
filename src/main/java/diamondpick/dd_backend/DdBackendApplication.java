4f1a400a6f0252b679027c231838a89329063bd4
evoLonation <1838940019@qq.com>
2022-06-09 20:40:32 +0800

    zzy：解决时区问题

--- src/main/java/diamondpick/dd_backend/DdBackendApplication.java
+++ src/main/java/diamondpick/dd_backend/DdBackendApplication.java
@@ -3,8 +3,16 @@ package diamondpick.dd_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DdBackendApplication {
+    @PostConstruct
+    void started() {
+        TimeZone timeZone = TimeZone.getTimeZone("Asia/Shanghai");
+        TimeZone.setDefault(timeZone);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DdBackendApplication.class, args);
