be7e7ab14c8597fdc7c96366318dd9e81d81e0c6
evoLonation <1838940019@qq.com>
2022-06-09 18:25:40 +0800

    zzy：新增推荐模板

--- src/test/java/diamondpick/dd_backend/zzy/DdBackendApplicationTests.java
+++ src/test/java/diamondpick/dd_backend/zzy/DdBackendApplicationTests.java
@@ -2,7 +2,6 @@ package diamondpick.dd_backend.zzy;
 
 import diamondpick.dd_backend.Dao.TemplateDao;
 import diamondpick.dd_backend.Entity.Document;
-import diamondpick.dd_backend.Old.zzy.Dao.CollectionDao;
 import diamondpick.dd_backend.Dao.DocumentDao;
 import diamondpick.dd_backend.Service.DocumentService;
 import diamondpick.dd_backend.Service.LocalFileService;
@@ -20,8 +19,6 @@ class DdBackendApplicationTests {
 
     @Autowired
     DocumentDao documentDao;
-    @Autowired
-    CollectionDao collectionDao;
 
     @Autowired
     DocumentService documentService;
